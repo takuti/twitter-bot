7a614a0a9e979e0d52084929a39057492449e5b2
takuti <k.takuti@gmail.com>
2013-07-16 14:33:03 +0900

    ツイート処理を別スクリプトに分割

---
+++ tweet.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+require 'twitter'
+require_relative 'markov'
+
+Twitter.configure do |config|
+	config.consumer_key = YOUR_CONSUMER_KEY
+	config.consumer_secret = YOUR_CONSUMER_SECRET
+	config.oauth_token = YOUR_OAUTH_TOKEN
+	config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+end
+
+markov = Markov.new
+
+Twitter.update(markov_tweet)
+puts '[tweet] ' + markov.generate_tweet
