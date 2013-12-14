b71fe7c5795bce0e27f605a49fed781a5e880496
takuti <k.takuti@gmail.com>
2013-12-14 16:25:48 +0900

    Corresponded to update of twitter ruby gem

--- tweet.rb
+++ tweet.rb
@@ -4,15 +4,15 @@ require 'twitter'
 require 'csv'
 require_relative 'markov'
 
-Twitter.configure do |config|
-	config.consumer_key = YOUR_CONSUMER_KEY
+rest = Twitter::REST::Client.new do |config|
+  config.consumer_key = YOUR_CONSUMER_KEY
 	config.consumer_secret = YOUR_CONSUMER_SECRET
 	config.oauth_token = YOUR_OAUTH_TOKEN
 	config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
 end
 
-tweets_table = CSV.table('tweets/tweets.csv')
+tweets_table = CSV.table('data/tweets/tweets.csv')
 markov = Markov.new(tweets_table[:text])
 
-# Twitter.update(markov_tweet)
+# rest.update(markov.generate_tweet)
 puts "[tweet] #{markov.generate_tweet}"
