aca7abf0869bdf66ac7b25fe12415742d8d7b700
takuti <k.takuti@gmail.com>
2014-11-23 14:21:48 +0900

    separate production/development cases in tweet.rb

--- tweet.rb
+++ tweet.rb
@@ -4,15 +4,17 @@ require 'twitter'
 require 'csv'
 require_relative 'markov'
 
-rest = Twitter::REST::Client.new do |config|
-  config.consumer_key = YOUR_CONSUMER_KEY
-  config.consumer_secret = YOUR_CONSUMER_SECRET
-  config.oauth_token = YOUR_OAUTH_TOKEN
-  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
-end
-
 tweets_table = CSV.table('data/tweets/tweets.csv')
 markov_table = create_markov_table(tweets_table[:text])
 
-# rest.update(generate_tweet(markov_table))
-puts "[tweet] #{generate_tweet(markov_table)}"
+if ARGV[0] == 'production'
+  rest = Twitter::REST::Client.new do |config|
+    config.consumer_key = YOUR_CONSUMER_KEY
+    config.consumer_secret = YOUR_CONSUMER_SECRET
+    config.oauth_token = YOUR_OAUTH_TOKEN
+    config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  end
+  rest.update(generate_tweet(markov_table))
+else
+  puts "[tweet] #{generate_tweet(markov_table)}"
+end
