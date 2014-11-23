f35fbf7ac64b1bf4f4ed40d137f5a46a184d386b
takuti <k.takuti@gmail.com>
2014-11-23 13:41:14 +0900

    replace tab -> spaces

--- tweet.rb
+++ tweet.rb
@@ -5,10 +5,10 @@ require 'csv'
 require_relative 'markov'
 
 rest = Twitter::REST::Client.new do |config|
-	config.consumer_key = YOUR_CONSUMER_KEY
-	config.consumer_secret = YOUR_CONSUMER_SECRET
-	config.oauth_token = YOUR_OAUTH_TOKEN
-	config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
+  config.consumer_key = YOUR_CONSUMER_KEY
+  config.consumer_secret = YOUR_CONSUMER_SECRET
+  config.oauth_token = YOUR_OAUTH_TOKEN
+  config.oauth_token_secret = YOUR_OAUTH_TOKEN_SECRET
 end
 
 tweets_table = CSV.table('data/tweets/tweets.csv')
