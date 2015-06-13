9dfcf158fd6da407ba2d4b40dacc5134c0e41c61
takuti <k.takuti@gmail.com>
2015-06-13 10:25:34 +0900

    add comments

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'twitter'
 require 'csv'
 require_relative 'markov'
 
+# read tweets from official tweet history
 tweets_table = Array.new
 CSV.foreach('data/tweets/tweets.csv', :headers => true) do |row|
   tweet = normalize_tweet(row['text'])
@@ -11,8 +12,11 @@ CSV.foreach('data/tweets/tweets.csv', :headers => true) do |row|
   tweets_table << tweet
 end
 
+# create markov table which has all 3-grams based on the tweets table
 markov_table = create_markov_table(tweets_table)
 
+# generate and tweet on twitter: `$ ruby main.rb production`
+# just generate tweet: `$ ruby main.rb`
 if ARGV[0] == 'production'
   rest = Twitter::REST::Client.new do |config|
     config.consumer_key = YOUR_CONSUMER_KEY
