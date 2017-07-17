7e5b1f7041c4414f20f8084c5461c05e55f5df8b
Takuya Kitazawa <k.takuti@gmail.com>
2017-07-18 07:04:31 +0900

    String

--- scripts/dict.rb
+++ scripts/dict.rb
@@ -13,8 +13,8 @@ cnt = 0
 
 case ARGV.first
 when 'hatena'
-  source_file = File.open("keywordlist_furigana.csv", "r:euc-jp")
-  dictionary_file = File.open("hatena.csv", "w:euc-jp")
+  source_file = File.open('keywordlist_furigana.csv', 'r:euc-jp')
+  dictionary_file = File.open('hatena.csv', 'w:euc-jp')
   while line = source_file.gets do
     # data = [<furigana>, <word>]
     data = NKF.nkf('-E -w',line).gsub(/\n/,'').split("\t")
@@ -22,8 +22,8 @@ when 'hatena'
     word = data[1]
     next if /^[0-9]{1,4}-[0-9]{2}-[0-9]{2}$/ =~ word # 2009-09-04 のような年月は飛ばす
     next if /^[0-9]{1,4}年$/ =~ word # 1945年　のような年は飛ばす
-    next if word.include?(",") # 単語そのものにカンマが含まれるものは飛ばす（応急処置）
-    next if tagger.wakati(word).size == 1 # すでに1単語として認識されるものは飛ばす\
+    next if word.include?(',') # 単語そのものにカンマが含まれるものは飛ばす（応急処置）
+    next if tagger.wakati(word).size == 1 # すでに1単語として認識されるものは飛ばす
 
     furigana = Moji.hira_to_kata(data[0])
 
@@ -40,14 +40,14 @@ when 'hatena'
   dictionary_file.close
   source_file.close
 when 'wikipedia'
-  source_file = File.open("jawiki-latest-all-titles", "r")
-  dictionary_file = File.open("wikipedia.csv", "w:euc-jp")
+  source_file = File.open('jawiki-latest-all-titles', 'r')
+  dictionary_file = File.open('wikipedia.csv', 'w:euc-jp')
   while word = source_file.gets do
     next if /^[0-9]{2}月[0-9]{2}日$/ =~ word # 09月04日 のような月日は飛ばす
     next if /^[0-9\*\-]{1,4}年.*$/ =~ word # 年から始まるものに使えそうなものはほぼ無い
-    next if word.include?("_") # 単語そのものにアンダーバーが含まれるものはスペースの入ったタイトルで複雑なので飛ばす（応急処置）
-    next if word.include?(",") # 単語そのものにカンマが含まれるものは飛ばす（応急処置）
-    next if tagger.wakati(word).size == 1 # すでに1単語として認識されるものは飛ばす\
+    next if word.include?('_') # 単語そのものにアンダーバーが含まれるものはスペースの入ったタイトルで複雑なので飛ばす（応急処置）
+    next if word.include?(',') # 単語そのものにカンマが含まれるものは飛ばす（応急処置）
+    next if tagger.wakati(word).size == 1 # すでに1単語として認識されるものは飛ば
 
     # 文字コードがCP51932になってしまうものは飛ばす（応急処置）
     dic_row_euc = NKF.nkf('-W -e',"#{word},0,0,#{cost(word)},名詞,一般,*,*,*,*,#{word},,")
